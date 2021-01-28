8c6a0302c43038314c060761ad8ece4993a5e37b
Pablo <pablohanry9@gmail.com>
2021-01-28 23:00:23 +0100

    test

--- src/components/MainPage/Header/Header.js
+++ src/components/MainPage/Header/Header.js
@@ -1,15 +1,11 @@
 import React from "react";
 import Logo from "../../../dist/img/SA.png";
-import Livre from "./Livre";
 import { Link } from "react-router-dom";
 
 const Header = () => (
   <div className="page p-5 d-flex container">
-    <div className="book">
-      <Livre />
-    </div>
     <div className="">
-      <div className="col-6 text-center float-right">
+      <div className="text-center">
         <img src={Logo} className="logo mx-auto d-block" alt="" />
         <h4 className="text-dark titre">
           Apprends un langage informatique gratuitement tout en t'amusant.
